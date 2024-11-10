055d21c12edbb173689f101a122dbe4adc43ea0e
eduardocv30 <jecruz@estudiantec.cr>
2024-11-09 19:49:40 -0600

    ligeros cambios en el cache

--- PE.cpp
+++ PE.cpp
@@ -15,8 +15,8 @@ private:
 
 public:
     // Constructor
-    PE(int _id) {
-        id = _id;
+    PE(int _id) : id(_id), cache(_id){
+        reg[3] = {0};
     }
 
     // Métodos para obtener los atributos
@@ -24,12 +24,20 @@ public:
         return id;
     }
 
+    int get_cache_id(){
+        return cache.get_id();
+    };
+
     uint64_t get_reg(int reg_num) {
         return reg[reg_num];
     }
 
     void set_reg(int reg_num, uint64_t value) { //metodo para definir el contenido de un registro
-        reg[reg_num] = value;
+        if (reg_num >= 0 && reg_num < 4) {  // Verificar que reg_num está entre 0 y 3
+            reg[reg_num] = value;
+        } else {
+            cout << "Error: índice de registro fuera de rango.\n";
+        }
     }
 
     void INC(int reg_num){ // incremneto en 1 del registro
@@ -40,8 +48,20 @@ public:
         reg[reg_num] = reg[reg_num] - 1;
     }
 
-    void WRITE(int reg_num, uint8_t addres){
-        cache.write(addres,reg[reg_num]);
+    void WRITE(int reg_num, uint8_t address){
+        if (reg_num >= 0 && reg_num < 4) {  // Verificar que reg_num está dentro de los límites
+            cache.write(address,reg[reg_num]); //escribe los valores en la cache
+        } else {
+            cout << "Error: índice de registro fuera de rango en READ.\n";
+        }
+    }
+
+    void READ(int reg_num, uint8_t address){
+        if (reg_num >= 0 && reg_num < 4) {  // Verificar que reg_num está dentro de los límites
+            reg[reg_num] = cache.read(address);  // Leer el valor de la caché y guardarlo en el registro
+        } else {
+            cout << "Error: índice de registro fuera de rango en READ.\n";
+        }
     }
 
 };
--- main.cpp
+++ main.cpp
@@ -6,18 +6,30 @@
 using namespace std;
 
 int main() {
-    PE core1(1);
-    PE core2(2);
-    PE core3(3);
-    PE core4(4);
+    PE core1=PE(1);
+    PE core2=PE(2);
+    PE core3=PE(3);
+    PE core4=PE(4);
 
     //Ram ram();
 
-    core1.set_reg(1,1);
-    core1.set_reg(2,2);
-    core1.set_reg(3,3);
-    core1.set_reg(4,4);
+    core1.set_reg(0,1);
+    core1.set_reg(1,2);
+    core1.set_reg(2,3);
+
     core1.WRITE(1,0);
+    core1.READ(3,0);
+    uint64_t x=core1.get_reg(3);
+
     core1.WRITE(2,0);
+    core1.READ(3,0);
+    uint64_t y=core1.get_reg(3);
+
     core1.WRITE(1,1);
+    core1.READ(3,1);
+    uint64_t z=core1.get_reg(3);
+
+    cout << "x = [" << x << "]\n";
+    cout << "y = [" << y << "]\n";
+    cout << "z = [" << z << "]\n";
 }
